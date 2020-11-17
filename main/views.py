81aab19ced0897fd4a9a46c01ae171d6edf6d6d2
palaash05 <agarwalpalaash@gmail.com>
2020-11-17 20:11:57 +0530

    Added csv download option

--- main/views.py
+++ main/views.py
@@ -1,9 +1,12 @@
+from django.http.response import StreamingHttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import request
 from .models import Banbury, Curing, Cutter, Fabric, Winder
 import json
 import time
+import csv
+from itertools import chain
 # Create your views here.
 
 '''
@@ -77,9 +80,9 @@ def dashboard_json(request):
     curData = Curing.objects.get(id=x)
 
     # Data dictionary for API
-    data = {"ban_width": banData.sheet_width,
-            "ban_date": str(banData.dateinserted), "fab_temp": fabData.temperature, "fab_date": str(fabData.dateinserted), "fab_w": fabData.sheet_width, "bead_temp": beadData.temperature,
-            "bead_date": str(beadData.dateinserted), "tread_pos": treadData.position_error, "tread_date": str(treadData.dateinserted), "tread_speed": treadData.speed_error, "cur_time": curData.molding_time_mins, "cur_date": str(curData.dateinserted), "cur_temp": curData.temperature}
+    data = {"dash_ban_width": banData.sheet_width,
+            "dash_ban_date": str(banData.dateinserted), "dash_fab_temp": fabData.temperature, "dash_fab_date": str(fabData.dateinserted), "dash_fab_w": fabData.sheet_width, "dash_bead_temp": beadData.temperature,
+            "dash_bead_date": str(beadData.dateinserted), "dash_tread_pos": treadData.position_error, "dash_tread_date": str(treadData.dateinserted), "dash_tread_speed": treadData.speed_error, "dash_cur_time": curData.molding_time_mins, "dash_cur_date": str(curData.dateinserted), "dash_cur_temp": curData.temperature}
 
     return HttpResponse(json.dumps(data))
 
@@ -131,12 +134,6 @@ def fabric_json(request):
 
     global fab, fabList1, fabList2, fabdateList, fabCount1, fabCount2
 
-    if len(fabList1) >= 10:
-        while len(fabList1) > 10:
-            fabdateList.pop(0)
-            fabList1.pop(0)
-            fabList2.pop(0)
-
     x = fab % 1000
     fab += 1
 
@@ -147,6 +144,12 @@ def fabric_json(request):
 
     fabList1.append(temp)
     fabList2.append(width)
+
+    if len(fabList1) >= 10:
+        while len(fabList1) > 10:
+            fabdateList.pop(0)
+            fabList1.pop(0)
+            fabList2.pop(0)
 
     if temp < 600 and temp > 500:
         fabCount1[0] += 1
@@ -178,11 +181,6 @@ def beadWinder_json(request):
 
     global bead, beadList, beaddateList, beadCount
 
-    if len(beadList) >= 10:
-        while len(beadList) > 10:
-            beaddateList.pop(0)
-            beadList.pop(0)
-
     x = bead % 1000
     bead += 1
 
@@ -190,6 +188,11 @@ def beadWinder_json(request):
 
     temp = beadData.temperature
     beadList.append(temp)
+
+    if len(beadList) >= 10:
+        while len(beadList) > 10:
+            beaddateList.pop(0)
+            beadList.pop(0)
 
     if temp < 700 and temp > 600:
         beadCount[0] += 1
@@ -214,12 +217,6 @@ def treadCutter_json(request):
 
     global tread, treadList1, treadList2, treaddateList, treadCount1, treadCount2
 
-    if len(treadList1) >= 10:
-        while len(treadList1) > 10:
-            treaddateList.pop(0)
-            treadList1.pop(0)
-            treadList2.pop(0)
-
     x = tread % 1000
     tread += 1
 
@@ -230,6 +227,12 @@ def treadCutter_json(request):
 
     treadList1.append(pos)
     treadList2.append(speed)
+
+    if len(treadList1) >= 10:
+        while len(treadList1) > 10:
+            treaddateList.pop(0)
+            treadList1.pop(0)
+            treadList2.pop(0)
 
     if pos > -2 and pos < 2:
         treadCount1[0] += 1
@@ -261,12 +264,6 @@ def Curing_json(request):
 
     global cur, curList1, curList2, curdateList, curCount1, curCount2
 
-    if len(curList1) >= 10:
-        while len(curList2) > 10:
-            curdateList.pop(0)
-            curList1.pop(0)
-            curList2.pop(0)
-
     x = cur % 1000
     cur += 1
 
@@ -277,6 +274,12 @@ def Curing_json(request):
 
     curList1.append(time)
     curList2.append(temp)
+
+    if len(curList1) >= 10:
+        while len(curList2) > 10:
+            curdateList.pop(0)
+            curList1.pop(0)
+            curList2.pop(0)
 
     if time > 30 and time < 35:
         curCount1[0] += 1
@@ -300,5 +303,36 @@ def Curing_json(request):
     return HttpResponse(json.dumps(data))
 
 
+class Echo:
+    @staticmethod
+    def write(value):
+        return value
+
+
 def stats(request):
-    return render(request, "main/stats.html")
+    global ban, fab, bead, tread, cur
+
+    header = (['Banbury Date&Time', 'Banbury Sheet Width', 'Fabric Calender Date&Time', 'Fabric Temperature', 'Fabric Width', 'Bead Winder Date&Time', 'Bead Temperature',
+               'Treading Cutter Date&Time', 'Tread Position Error', 'Tread Speed Error', 'Curing Machine Date&Time', 'Curing Moulding Temperature', 'Curing Moulding Time'],)
+
+    Data_csv = []
+
+    for (a, b, c, d, e) in zip(range(1, ban+1), range(1, fab+1), range(1, bead+1), range(1, tread+1), range(1, cur+1)):
+
+        banData = Banbury.objects.get(id=a)
+        fabData = Fabric.objects.get(id=b)
+        beadData = Winder.objects.get(id=c)
+        treadData = Cutter.objects.get(id=d)
+        curData = Curing.objects.get(id=e)
+
+        data = ([str(banData.dateinserted), banData.sheet_width, str(fabData.dateinserted), fabData.temperature, fabData.sheet_width, str(beadData.dateinserted),
+                 beadData.temperature, str(treadData.dateinserted), treadData.position_error, treadData.speed_error, str(curData.dateinserted), curData.temperature, curData.molding_time_mins])
+        Data_csv.append(data)
+
+    psuedo_buffer = Echo()
+    writer = csv.writer(psuedo_buffer)
+    response = StreamingHttpResponse(
+        (writer.writerow(row) for row in chain(header, Data_csv)), content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="Current_Report.csv"'
+    response['status'] = 200
+    return response
